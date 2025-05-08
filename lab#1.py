36565283129403ed9c7ee0649fb5621c5de8bda5
Areeba-Rehman12 <rhmnareeba@gmail.com>
2025-05-08 08:02:46 -0700

    Update lab#1.py

--- lab#1.py
+++ lab#1.py
@@ -3,7 +3,7 @@ import padas as pd
 import numpy as np
 from sklearn import linear_model
 x=np.array{(5,14,25,35,45,55)}.reshape((-1,-1))
-y=bp.array{(5,20,14,32,22,38)}
+y=np.array{(5,20,14,32,22,38)}
 model=linear_model.linear regression()
 model fit(x,y)
 x-new=np.array([60]).reshape((-1,1))

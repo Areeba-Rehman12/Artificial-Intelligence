d4c7fc59729ece59b4a19f9fd50b70e918960396
Areeba <rhmnareeba@gmail.com>
2025-05-09 20:57:31 -0700

    lab#1

--- lab#1.py
+++ lab#1.py
@@ -1,11 +1,11 @@
-import sklearn
-import padas as pd
-import numpy as np
-from sklearn import linear_model
-x=np.array{(5,14,25,35,45,55)}.reshape((-1,-1))
-y=np.array{(5,20,14,32,22,38)}
-model=linear_model.linear regression()
-model fit(x,y)
+from hashlib import new
+import padas as pd # type: ignore
+import numpy as np # type: ignore
+from sklearn import linear_model # type: ignore
+x=np.array[(5,14,25,35,45,55)]#.reshape((-1,-1))
+y=np.array[(5,20,14,32,22,38)]
+model=linear_model.LinearRegression()
+model.fit(x,y)
 x-new=np.array([60]).reshape((-1,1))
 y-new=model.predict(x-new)
 print(y-new)
